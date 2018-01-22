bca8314858bbb8007dc325651ed60a067bcfdeaf
alessio <klodale9491@gmail.com>
2018-01-22 23:59:50 +0100

    Crawler funzionante

--- Main.py
+++ Main.py
@@ -3,7 +3,6 @@ from WebCrawler import MyWebCrawler
 
 we = MyWebCrawler()
 fp = open("links.txt","w")
-we.exploreTree("http://ricette.giallozafferano.it/",[],0,fp)
+# we.getSitemap("http://ricette.giallozafferano.it/",0,fp)
+we.getSitemap2("http://ricette.giallozafferano.it/")
 print(we.to_visit)
-#ingredienti = we.extractWords()
-#print(ingredienti)
