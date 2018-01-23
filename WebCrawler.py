8980a89a8c05f5e651f1a6209372952263019dc4
alessio <klodale9491@gmail.com>
2018-01-23 03:58:23 +0100

    Pulito WebCrawler

--- WebCrawler.py
+++ WebCrawler.py
@@ -7,49 +7,22 @@ import re
 
 class MyWebCrawler:
     def __init__(self):
-        self.to_visit = []
+        pass
 
-    def getIngredients(self):
-        ingredients = []
-        r = urllib.request.urlopen(self.url).read()
-        soup = BeautifulSoup(r)
-        htmlElements = soup.find_all("dd", class_="ingredient")
-
-        for el in  htmlElements:
-            ingredients.append(el.a.get_text())
-
-        return ingredients
-
-    def getSitemap(self, url, e, fp):
-        try:
-            html_page = urllib.request.urlopen(url)
-            soup = BeautifulSoup(html_page)
-            links = soup.findAll('a', attrs={'href': re.compile("^http://.*giallozafferano\.it")})
-
-            for link in links:
-                if link.get("href") not in self.to_visit:
-                    self.to_visit.append(link.get("href"))
-                    fp.write(link.get("href") + "\n")
-                    print(link.get("href"))
-            self.exploreTree(self.to_visit[e], e + 1, fp)
-        except Exception:
-            print("Waiting....")
-            sleep(10)
-            self.exploreTree(self.to_visit[e], e + 1, fp)
-
-    def getSitemap2(self,url):
-        fp = open("links2.txt","w")
+    def setRicetteLinks(self,url):
         coda = []
         all_links = []
+        max_attempts = 10
+        count_attempt = 1
+
+        fp = open("links_ricetta.txt", "w")
         fp.write(url+"\n")
         coda.append(url)
-
         while len(coda) >= 1:
             try:
                 html_page = urllib.request.urlopen(coda[0])
                 soup = BeautifulSoup(html_page)
                 links = soup.findAll('a', attrs={'href': re.compile("^http://.*giallozafferano\.it")})
-
                 for link in links:
                     text_link = link.get("href")
                     if text_link not in all_links:
@@ -57,22 +30,20 @@ class MyWebCrawler:
                         coda.append(text_link)
                         if re.match("^http://ricette\.giallozafferano\.it",text_link):
                             fp.write(text_link + "\n")
-                        #if re.match("^http://ricette\.giallozafferano\.it.*/related/$",text_link):
-                            #fp.write(text_link + "\n")
                         print(text_link)
                 # Rimuovo dalla coda il link radice
                 del(coda[0])
-
-
             except HTTPError as Error:
-                if Error.code == 404:
+                if Error.code != 502:
                     del(coda[0])
                     continue
-                elif Error.code == 502:
-                    print("Waiting...")
-                    sleep(10)
-                else:
-                    raise(Exception)
+                else :
+                    if count_attempt < max_attempts:
+                        print("Waiting " + str(count_attempt * 10) + "seconds")
+                        sleep(count_attempt * 10)
+                        count_attempt += 1
+                    else:
+                        break
             except Exception:
                 del(coda[0])
                 continue
