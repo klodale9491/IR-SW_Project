6be9c1cfc3d27493c296d18f1ae886d29431a6f8
alessio <klodale9491@gmail.com>
2018-01-24 01:16:58 +0100

    Main per testate TreeTagger

--- Main.py
+++ Main.py
@@ -8,5 +8,18 @@ print(r.category)
 print(r.subCategory)
 '''
 
+
+'''
+import treetaggerwrapper
+from pprint import pprint
+
+
+it_string = "Tonno sott'olio sgocciolato"
+it_string = it_string.replace("\t","")
+tagger = treetaggerwrapper.TreeTagger(TAGLANG="it")
+tags = tagger.tag_text(it_string)
+pprint(treetaggerwrapper.make_tags(tags))
+'''
+
 wc = MyWebCrawler()
 wc.loadDBRicette()
